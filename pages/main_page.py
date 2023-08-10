bccafecd4033a42019b3f903a2f5ff6585cca47b
Balaji <balaji0017@gmail.com>
2023-08-10 10:55:49 +0530

    Created Page Objects

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,12 @@
+from selenium.webdriver.common.by import By
+
+
 class MainPage:
     def __init__(self, driver):
         self.driver = driver
 
     def get_main_page_title(self):
         return self.driver.title
+
+    def click_on_patient_menu(self):
+        self.driver.find_element(By.XPATH, "//div[text()='Patient']").click()
